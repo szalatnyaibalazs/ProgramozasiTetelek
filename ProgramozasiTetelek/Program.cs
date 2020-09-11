e5e62f5454a2d00f003cbc569e00e63e13365922
szalatnyaibalazs <szalatnyaibalazs@gamil.com>
2020-09-11 10:16:12 +0200

    Előkészítés

--- ProgramozasiTetelek/Program.cs
+++ ProgramozasiTetelek/Program.cs
@@ -8,9 +8,22 @@ namespace ProgramozasiTetelek
 {
     class Program
     {
+        static int[] szamok = new int[] { 12, 7, 2, 4, 45, 99, 1, 8, 95, 50 };
+
+        private static void TombKiiras()
+        {
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write("{0}, ",szamok[i]);
+            }
+        }
         static void Main(string[] args)
         {
+            TombKiiras();
 
+            Console.ReadKey();
         }
+
+        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+#Programozási tételek
 
+## Elõkészítés
+- 10 elemû egész számot tartalmazó tömb létrehozása
+- A tömb elemei:
+  - 12, 7, 2, 4, 45, 99, 1, 8, 95, 50
+- A tömb az összes metódusban látható legyen
+- TömbKiirás() metódus létrehozása 
+  - Nem ad vissza értéket
+  - Nincs paramétere
+  - A tömb elemeit egy sorba vesszõvel elválasztva console-ra írja 
